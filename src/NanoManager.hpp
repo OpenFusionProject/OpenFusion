d618d1b35362c7f3dfdea43a3952a78e908a6f34
Onii-chan <failer007@live.dk>
2020-08-22 17:11:41 +0200

    Updated indentations

--- src/NanoManager.hpp
+++ src/NanoManager.hpp
@@ -4,18 +4,18 @@
 #include "CNShardServer.hpp"
 
 namespace NanoManager {
-	void init();
-	void nanoSummonHandler(CNSocket* sock, CNPacketData* data);
-	void nanoEquipHandler(CNSocket* sock, CNPacketData* data);
-	void nanoUnEquipHandler(CNSocket* sock, CNPacketData* data);
-	void nanoGMGiveHandler(CNSocket* sock, CNPacketData* data);
-	void nanoSkillUseHandler(CNSocket* sock, CNPacketData* data);
-	void nanoSkillSetHandler(CNSocket* sock, CNPacketData* data);
+    void init();
+    void nanoSummonHandler(CNSocket* sock, CNPacketData* data);
+    void nanoEquipHandler(CNSocket* sock, CNPacketData* data);
+    void nanoUnEquipHandler(CNSocket* sock, CNPacketData* data);
+    void nanoGMGiveHandler(CNSocket* sock, CNPacketData* data);
+    void nanoSkillUseHandler(CNSocket* sock, CNPacketData* data);
+    void nanoSkillSetHandler(CNSocket* sock, CNPacketData* data);
 
-	// Helper methods
-	void addNano(CNSocket* sock, int16_t nanoId, int16_t slot);
-	void setNanoSkill(CNSocket* sock, int16_t nanoId, int16_t skillId);
-	void resetNanoSkill(CNSocket* sock, int16_t nanoId);
+    // Helper methods
+    void addNano(CNSocket* sock, int16_t nanoId, int16_t slot);
+    void setNanoSkill(CNSocket* sock, int16_t nanoId, int16_t skillId);
+    void resetNanoSkill(CNSocket* sock, int16_t nanoId);
 }
 
 #endif
