377c8ab5129a38b4be7ab48b3b095bb106fa9de5
CPunch <sethtstubbs@gmail.com>
2020-08-22 22:59:44 -0500

    Update ChatManager.cpp

--- src/ChatManager.cpp
+++ src/ChatManager.cpp
@@ -34,11 +34,11 @@ void ChatManager::menuChatHandler(CNSocket* sock, CNPacketData* data) {
     PlayerView plr = PlayerManager::players[sock];
 
     // send to client
-    sP_FE2CL_REP_SEND_MENUCHAT_MESSAGE_SUCC* resp = (sP_FE2CL_REP_SEND_MENUCHAT_MESSAGE_SUCC*)xmalloc(sizeof(sP_FE2CL_REP_SEND_MENUCHAT_MESSAGE_SUCC));
-    memcpy(resp->szFreeChat, chat->szFreeChat, sizeof(chat->szFreeChat));
-    resp->iPC_ID = PlayerManager::players[sock].plr.iID;
-    resp->iEmoteCode = chat->iEmoteCode;
-    sock->sendPacket(new CNPacketData((void*)resp, P_FE2CL_REP_SEND_MENUCHAT_MESSAGE_SUCC, sizeof(sP_FE2CL_REP_SEND_MENUCHAT_MESSAGE_SUCC), sock->getFEKey()));
+    INITSTRUCT(sP_FE2CL_REP_SEND_MENUCHAT_MESSAGE_SUCC, resp);
+    memcpy(resp.szFreeChat, chat->szFreeChat, sizeof(chat->szFreeChat));
+    resp.iPC_ID = PlayerManager::players[sock].plr.iID;
+    resp.iEmoteCode = chat->iEmoteCode;
+    sock.sendPacket((void*)&resp, P_FE2CL_REP_SEND_MENUCHAT_MESSAGE_SUCC, sizeof(sP_FE2CL_REP_SEND_MENUCHAT_MESSAGE_SUCC));
 
     // send to visible players
     for (CNSocket* otherSock : plr.viewable) {
