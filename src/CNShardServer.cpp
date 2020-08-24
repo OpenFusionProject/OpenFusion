16bca39dae12d1c4767c118b7b5a1c63ca32dc5d
CPunch <sethtstubbs@gmail.com>
2020-08-24 16:11:40 -0500

    added simple timer system to CNShardServer

--- src/CNShardServer.cpp
+++ src/CNShardServer.cpp
@@ -10,10 +10,12 @@
 #include <cstdlib>
 
 std::map<uint32_t, PacketHandler> CNShardServer::ShardPackets;
+std::list<TimerEvent> CNShardServer::Timers;
 
 CNShardServer::CNShardServer(uint16_t p) {
     port = p;
     pHandler = &CNShardServer::handlePacket;
+    REGISTER_SHARD_TIMER(keepAliveTimer, 2000);
     init();
 }
 
@@ -24,6 +26,22 @@ void CNShardServer::handlePacket(CNSocket* sock, CNPacketData* data) {
         ShardPackets[data->type](sock, data);
     else if (settings::VERBOSITY)
         std::cerr << "OpenFusion: SHARD UNIMPLM ERR. PacketType: " << Defines::p2str(CL2FE, data->type) << " (" << data->type << ")" << std::endl;
+}
+
+void CNShardServer::keepAliveTimer(CNServer* serv,  uint64_t currTime) {
+    std::cout << "keep alive called! " << currTime << std::endl;
+    auto cachedPlayers = PlayerManager::players;
+
+    for (auto pair : cachedPlayers) {
+        if (pair.second.lastHeartbeat != 0 && currTime - pair.second.lastHeartbeat > 4000) { // if the client hadn't responded in 4 seconds, its a dead connection so throw it out
+            pair.first->kill();
+            continue;
+        }
+
+        // passed the heartbeat, send another
+        INITSTRUCT(sP_FE2CL_REQ_LIVE_CHECK, data);
+        pair.first->sendPacket((void*)&data, P_FE2CL_REQ_LIVE_CHECK, sizeof(sP_FE2CL_REQ_LIVE_CHECK));
+    }
 }
 
 void CNShardServer::newConnection(CNSocket* cns) {
@@ -38,19 +56,20 @@ void CNShardServer::killConnection(CNSocket* cns) {
     CNSharedData::erasePlayer(cachedPlr.SerialKey);
 }
 
-void CNShardServer::onTimer() {
+void CNShardServer::onStep() {
     uint64_t currTime = getTime();
 
-    auto cachedPlayers = PlayerManager::players;
-
-    for (auto pair : cachedPlayers) {
-        if (pair.second.lastHeartbeat != 0 && currTime - pair.second.lastHeartbeat > 4000) { // if the client hadn't responded in 4 seconds, its a dead connection so throw it out
-            pair.first->kill();
+    for (TimerEvent& event : Timers) {
+        if (event.scheduledEvent == 0) {
+            // event hasn't been queued yet, go ahead and do that
+            event.scheduledEvent = currTime + event.delta;
             continue;
         }
 
-        // passed the heartbeat, send another
-        INITSTRUCT(sP_FE2CL_REQ_LIVE_CHECK, data);
-        pair.first->sendPacket((void*)&data, P_FE2CL_REQ_LIVE_CHECK, sizeof(sP_FE2CL_REQ_LIVE_CHECK));
+        if (event.scheduledEvent < currTime) {
+            // timer needs to be called
+            event.handlr(this, currTime);
+            event.scheduledEvent = currTime + event.delta;
+        }
     }
 }
