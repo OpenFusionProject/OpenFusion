299fc1b461745412301dbb956d664a40220bf8a7
Jade <jadeshrinemaiden@gmail.com>
2020-11-08 00:26:44 +0000

    Refactored Nano Powers
    * All nano power functions have been merged into one goliath of a function.
    * Nano powers consume the correct amount of stamina.
    * Bugfixed gumball issues, gumballed nanos now perform better.
    * Revive powers now work correctly.
    * Recall powers both self and group are functional.
    * Removed nanoBuff.
    * Added a new applyBuff function, this allows for quick and easy application of nano skills.
    * Numerous other bugfixes.

--- src/Player.hpp
+++ src/Player.hpp
@@ -36,10 +36,12 @@ struct Player {
     int32_t iWarpLocationFlag;
     int64_t aSkywayLocationFlag[2];
     int32_t iConditionBitFlag;
+    int32_t iSelfConditionBitFlag;
     int8_t iSpecialState;
 
     int x, y, z, angle;
     int lastX, lastY, lastZ, lastAngle;
+    int recallX, recallY, recallZ, recallInstance;
     uint64_t instanceID;
     sItemBase Equip[AEQUIP_COUNT];
     sItemBase Inven[AINVEN_COUNT];
@@ -51,7 +53,7 @@ struct Player {
 
     bool inCombat;
     bool onMonkey;
-    bool passiveNanoOut;
+    int nanoDrainRate;
     int healCooldown;
 
     int pointDamage;
