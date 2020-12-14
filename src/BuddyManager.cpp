156e9bf9027f6d8c6237219dde7c958c31d9e7cb
Gent S <gentsemaj@live.com>
2020-12-13 20:18:36 -0500

    Fix newline characters being stripped from emails

--- src/BuddyManager.cpp
+++ src/BuddyManager.cpp
@@ -767,7 +767,7 @@ void BuddyManager::emailSend(CNSocket* sock, CNPacketData* data) {
         U16toU8(plr->PCStyle.szFirstName), // SenderFirstName
         U16toU8(plr->PCStyle.szLastName), // SenderLastName
         ChatManager::sanitizeText(U16toU8(pkt->szSubject)), // SubjectLine
-        ChatManager::sanitizeText(U16toU8(pkt->szContent)), // MsgBody
+        ChatManager::sanitizeText(U16toU8(pkt->szContent), true), // MsgBody
         pkt->iCash, // Taros
         (uint64_t)getTimestamp(), // SendTime
         0 // DeleteTime (unimplemented)
